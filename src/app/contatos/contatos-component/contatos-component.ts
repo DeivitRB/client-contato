39a640256a56a7438276218cbfe381c46685e390
DeivitRB <deivit@xpert.com.br>
2024-04-23 10:28:15 -0300

    .

--- src/app/contatos/contatos-component/contatos-component.ts
+++ src/app/contatos/contatos-component/contatos-component.ts
@@ -1,127 +1,237 @@
-import { Component, OnInit } from "@angular/core";
-import { faFilter, faPlus, faTimes, faArrowUpWideShort, faArrowDownWideShort } from "@fortawesome/free-solid-svg-icons";
-import { ContatosService } from "../contatos.service";
-import { FormGroup, FormBuilder } from "@angular/forms";
-import { filtroContatos } from "../interfaces";
-import Swal from "sweetalert2";
-
+import { Component, OnInit } from '@angular/core';
+import {
+  faFilter,
+  faPlus,
+  faTimes,
+  faArrowUpWideShort,
+  faArrowDownWideShort,
+  faAnglesRight,
+  faAnglesLeft,
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { ContatosService } from '../contatos.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { filtroContatos } from '../interfaces';
+import Swal from 'sweetalert2';
 
 @Component({
-    templateUrl: './contatos-component.html',
-    styleUrls: ['./contatos-component.css']
+  templateUrl: './contatos-component.html',
+  styleUrls: ['./contatos-component.css'],
 })
-
 export class contatosComponent implements OnInit {
-    faPlus = faPlus;
-    faFilter = faFilter;
-    faTimes = faTimes;
-    faArrowUpWideShort = faArrowUpWideShort;
-    faArrowDownWideShort = faArrowDownWideShort
-  
-    formFilter: FormGroup;
-    formContatoGrupos: FormGroup
-  
-    listHeader: any = [
-      {
-        label: 'Código',
-        prop: 'id',
-        toggle: false
-      },
-      {
-        label: 'Nome',
-        prop: 'ds_contato',
-        toggle: false
-      },
-      {
-        label: 'E-mail',
-        prop: 'ds_email',
-        toggle: false
-      },
-      {
-        label: 'Celular',
-        prop: 'nr_celular',
-        toggle: false
-      },
-      {
-        label: 'Status',
-        prop: 'st_ativo',
-        toggle: false
-      }
-    ]
-  
-    listContatos: any[] = []
-    page: number = 1
-    pageSize: number = 15;
-    showFilter: boolean = false;
-    loadingInline: boolean = false;
-    totalAtivos: number = 0;
-    totalInativos: number = 0
-    validMenuContatoGrupo: boolean = false
-  
-    constructor(private contatosServ: ContatosService, private fb: FormBuilder) {}
-  
-    ngOnInit() {
-      this.fnInitForm()
-      this.fnGetContatos()
-    }
-  
-    fnInitForm(): void {
-      this.formFilter = this.fb.group({
-        slctStatus: ['99'],
-        iptBuscar: ['']
+  faPlus = faPlus;
+  faFilter = faFilter;
+  faTimes = faTimes;
+  faArrowUpWideShort = faArrowUpWideShort;
+  faArrowDownWideShort = faArrowDownWideShort;
+  faAnglesRight = faAnglesRight;
+  faAnglesLeft = faAnglesLeft;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+
+  formFilter: FormGroup;
+  formContatoGrupos: FormGroup;
+
+  listHeader: any = [
+    {
+      label: 'Código',
+      prop: 'id',
+      toggle: false,
+    },
+    {
+      label: 'Nome',
+      prop: 'ds_contato',
+      toggle: false,
+    },
+    {
+      label: 'E-mail',
+      prop: 'ds_email',
+      toggle: false,
+    },
+    {
+      label: 'Celular',
+      prop: 'nr_celular',
+      toggle: false,
+    },
+    {
+      label: 'Status',
+      prop: 'st_ativo',
+      toggle: false,
+    },
+  ];
+
+  listContatos: any[] = [];
+  page: number = 1;
+  pageSize: number = 15;
+  showFilter: boolean = false;
+  loadingInline: boolean = false;
+  totalAtivos: number = 0;
+  totalInativos: number = 0;
+  validMenuContatoGrupo: boolean = false;
+  listGrupos: any = [];
+  listGruposContato: any = [];
+
+  constructor(private contatosServ: ContatosService, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.fnInitForm();
+    this.fnGetGrupos();
+    this.fnGetContatos();
+  }
+
+  fnInitForm(): void {
+    this.formFilter = this.fb.group({
+      slctStatus: ['99'],
+      iptBuscar: [''],
+    });
+
+    this.formContatoGrupos = this.fb.group({
+      iptNomeContato: [''],
+      iptEmailContato: [''],
+      iptCelularContato: [''],
+      checkStatusContato: [true],
+      idContato: [0],
+    });
+  }
+
+  async fnGetGrupos() {
+    (await this.contatosServ.getGrupos()).subscribe((resp: any) => {
+      this.listGrupos = resp;
+      this.listGrupos.map((el: any) => {
+        el.showGrupo = true;
       });
+      console.log(resp);
+    });
+  }
 
-      this.formContatoGrupos = this.fb.group({
-        iptNomeContato: [''],
-        iptEmailContato: [''],
-        iptCelularContato: [''],
-        checkStatusContato: [true]
-      })
-    }
-  
-    async fnGetContatos() {
-      this.totalAtivos = 0
-      this.totalInativos = 0
+  async fnGetContatos() {
+    this.totalAtivos = 0;
+    this.totalInativos = 0;
 
-      let objFiltro: filtroContatos = {
-        status: this.formFilter.get('slctStatus').value,
-        buscar: this.formFilter.get('iptBuscar').value
-      };
-  
-      this.loadingInline = true;
-      (await this.contatosServ.fnGetContatos(objFiltro)).subscribe((resp: any) => {
+    let objFiltro: filtroContatos = {
+      status: this.formFilter.get('slctStatus').value,
+      buscar: this.formFilter.get('iptBuscar').value,
+    };
+
+    this.loadingInline = true;
+    (await this.contatosServ.fnGetContatos(objFiltro)).subscribe(
+      (resp: any) => {
         this.listContatos = resp[0];
 
         this.listContatos.filter((fi: any) => {
-            if(fi.st_ativo == 1) {
-                this.totalAtivos += 1
-            }
-
-            if(fi.st_ativo == 0) {
-                this.totalInativos += 1
-            }
-        })
-
-        this.loadingInline = false;
-      }, (err) => {
-        Swal.fire('Ops!', 'Não foi possível carregar contatos.', 'error')
-        this.loadingInline = false;
-      })
-    }
-
-    sort(colName, toglee: boolean) {
-        this.listContatos.sort(
-          (a: any, b: any) => {
-            if (toglee == false) {
-              if (a[colName] < b[colName]) return -1;
-              if (a[colName] > b[colName]) return 1;
-              return 0;
-            } else {
-              if (a[colName] < b[colName]) return 1;
-              if (a[colName] > b[colName]) return -1;
-              return 0;
-            }
+          if (fi.st_ativo == 1) {
+            this.totalAtivos += 1;
           }
+
+          if (fi.st_ativo == 0) {
+            this.totalInativos += 1;
+          }
+        });
+
+        this.loadingInline = false;
+      },
+      (err) => {
+        Swal.fire('Ops!', 'Não foi possível carregar contatos.', 'error');
+        this.loadingInline = false;
+      }
+    );
+  }
+
+  sort(colName, toglee: boolean) {
+    this.listContatos.sort((a: any, b: any) => {
+      if (toglee == false) {
+        if (a[colName] < b[colName]) return -1;
+        if (a[colName] > b[colName]) return 1;
+        return 0;
+      } else {
+        if (a[colName] < b[colName]) return 1;
+        if (a[colName] > b[colName]) return -1;
+        return 0;
+      }
+    });
+  }
+
+  fnResetFormContato() {
+    this.listGruposContato = [];
+    this.validMenuContatoGrupo = false;
+    this.formContatoGrupos.patchValue({
+      iptNomeContato: [''],
+      iptEmailContato: [''],
+      iptCelularContato: [''],
+      checkStatusContato: [true],
+      idContato: [0],
+    });
+  }
+
+  async fnGetDadosContato(objContato: any) {
+    (await this.contatosServ.fnGetContato(objContato.id)).subscribe(
+      (resp: any) => {
+        this.formContatoGrupos.patchValue({
+          iptNomeContato: resp.dsContato,
+          iptEmailContato: resp.dsEmail,
+          iptCelularContato: resp.nrCelular,
+          checkStatusContato: resp.stAtivo == 1 ? true : false,
+          idContato: resp.id,
+        });
+
+        this.fnGetGruposContato(objContato.id);
+      },
+      (err) => {
+        Swal.fire(
+          'Ops!',
+          'Não foi possível carregar informações do contato.',
+          'error'
         );
+      }
+    );
+  }
+
+  async fnGetGruposContato(idContato: number) {
+    (await this.contatosServ.fnGetGruposContato(idContato)).subscribe(
+      (resp: any) => {
+        this.listGruposContato = resp[0];
+
+        if (this.listGruposContato.length > 0) {
+          this.listGruposContato.filter((fi: any) => {
+            this.listGrupos.map((fiG: any) => {
+              if (fi.grupocontato_id == fiG.id) {
+                fiG.showGrupo = false;
+              }
+            });
+          });
+        }
+      }
+    );
+  }
+
+  fnInserirGrupoContato(itemGrupo: any, i: number) {
+    if (itemGrupo.stAtivo == 0) {
+      Swal.fire('Ops!', 'Não é permitido inserir grupo inativo.', 'warning');
+      return;
     }
+
+    let objGrupoContato = {
+      contato_id: 0,
+      grupocontato_id: itemGrupo.id,
+      ds_grupocontato: itemGrupo.dsGrupocontato,
+    };
+    this.listGruposContato.push(objGrupoContato);
+
+    this.listGrupos[i].showGrupo = false;
+  }
+
+  fnInserirTodos() {
+    this.listGrupos.map((el: any) => {});
+  }
+
+  fnRetirarGrupodeContato(itemGrupo: any, i: number) {
+    this.listGrupos.filter((el: any) => {
+      if (itemGrupo.grupocontato_id == el.id) {
+        el.showGrupo = true;
+      }
+    });
+
+    this.listGruposContato.splice(i, 1);
+  }
 }
--- src/app/contatos/contatos.service.ts
+++ src/app/contatos/contatos.service.ts
@@ -1,36 +1,44 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { filtroContatos } from "./interfaces";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { filtroContatos } from './interfaces';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-
-
 export class ContatosService {
+  URL = 'http://127.0.0.1:3333';
 
-    URL = 'http://127.0.0.1:3333'
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {}
+  async fnGetContatos(objFiltro: filtroContatos) {
+    let params = new HttpParams();
+    Object.keys(objFiltro).forEach((key) => {
+      params = params.append(key, objFiltro[key]);
+    });
 
-    async fnGetContatos(objFiltro: filtroContatos) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept',
+      }),
+      params: params,
+    };
 
-      let params = new HttpParams();
-      Object.keys(objFiltro).forEach(key => {
-        params = params.append(key, objFiltro[key]);
-      });
+    return await this.http.get<any>(this.URL + `/contatos`, httpOptions);
+  }
 
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'Content-Type': 'application/json; charset=utf-8',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE',
-              'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-            }),
-            params: params
-          
-          };
-          
-        return await this.http.get<any>(this.URL + `/contatos`, httpOptions);
-    }
+  async fnGetContato(id: number) {
+    return await this.http.get<any>(this.URL + `/contatos/${id}`);
+  }
+
+  async getGrupos() {
+    return await this.http.get<any>(this.URL + `/group`);
+  }
+
+  async fnGetGruposContato(id: number) {
+    return await this.http.get<any>(this.URL + `/grupocontatos/${id}`);
+  }
 }
